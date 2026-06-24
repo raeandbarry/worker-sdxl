fa2fa788d6b4c4a120c9d6887326481f4df26df6
raeandbarry <rae@stillus.ai>
2026-06-24 03:33:21 -0700

    Update handler.py

--- handler.py
+++ handler.py
@@ -38,7 +38,7 @@ class ModelHandler:
         vae = AutoencoderKL.from_pretrained(
             "madebyollin/sdxl-vae-fp16-fix",
             torch_dtype=torch.float16,
-            local_files_only=False,
+            local_files_only=True,  # Use weights baked into the Docker image
         )
         base_pipe = StableDiffusionXLPipeline.from_pretrained(
             "SG161222/RealVisXL_V4.0",
@@ -46,7 +46,7 @@ class ModelHandler:
             torch_dtype=torch.float16,
             use_safetensors=True,
             add_watermarker=False,
-            local_files_only=False,
+            local_files_only=True,  # Use weights baked into the Docker image
         ).to("cuda")
 
         base_pipe.enable_xformers_memory_efficient_attention()
